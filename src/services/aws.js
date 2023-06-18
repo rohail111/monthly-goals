c1cb85fe4f001f8aed3a6ef64dd1c8c44dbb8054
Rohail Abbas <58682834+rohail111@users.noreply.github.com>
2023-06-18 16:27:10 +0500

    s3 changes

--- src/services/aws.js
+++ src/services/aws.js
@@ -1,34 +1,3 @@
-// import {S3} from '@aws-sdk/client-s3';
-// import {Upload} from '@aws-sdk/lib-storage';
-// import {Buffer} from 'buffer';
-
-// Buffer.from('anything', 'base64');
-// window.Buffer = window.Buffer || require('buffer').Buffer;
-
-// const fileUploader = (file, path) => {
-//   const creds = {
-//     region: 'us-east-2',
-//     credentials: {
-//       accessKeyId: 'AKIAQEXASG7F6QIACB4I',
-//       secretAccessKey: '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',
-//     },
-//   };
-//   const ReactS3Client = new S3(creds);
-
-//   return new Upload({
-//     client: ReactS3Client,
-//     params: {
-//       Bucket: 'monthly-goals',
-//       Key: path,
-//       Body: file,
-//     },
-//     queueSize: 4,
-//     partSize: 1024 * 1024 * 5,
-//     leavePartsOnError: false,
-//   });
-// };
-// export default fileUploader;
-
 import {S3} from '@aws-sdk/client-s3';
 import {Upload} from '@aws-sdk/lib-storage';
 import {Buffer} from 'buffer';
